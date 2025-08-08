0c377f58a2eccab07d288aef33f8361b3996cc0c
deepanshupawar <deepanshupawar@localhost>
2025-08-08 14:17:41 +0530

    initial commit

---
+++ First/src/com/nt/main/Main.java
@@ -0,0 +1,10 @@
+package com.nt.main;
+
+public class Main {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to my Frist GitPractice Project ::");
+		
+	}
+
+}
---
+++ GitPractice/src/com/nt/main/Main.java
@@ -0,0 +1,19 @@
+package com.nt.main;
+
+import com.nt.logic.Arithmetic;
+
+public class Main {
+	public static void main(String[] args) {
+		int a =10;
+		int b=20;
+		
+	   
+	   
+	   int addition = Arithmetic.sum(a, b);
+	   System.out.println(addition);
+	 
+	}
+	
+	
+	
+}	
